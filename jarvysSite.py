e939ae925b5c59d955d9d1b6920507fe6078a1e1
Nicolas Goureau <nicolas.goureau@lengow.com>
2018-01-23 11:59:37 +0100

    Really adding the route for PWA

--- jarvysSite.py
+++ jarvysSite.py
@@ -2,9 +2,18 @@ from flask import Flask,render_template
 
 myapp = Flask(__name__)
 
-@myapp.route('/')
+@app.route('/')
 def hello_world():
 	return render_template('Jarvys.html')
+
+@app.route('/pwa')
+def pwa():
+	return render_template('index.html')
+
+@app.route('/pwa/sw.js', methods=['GET'])
+def sw():
+    return app.send_static_file('sw.js')
+
 
 if __name__ == "__main__":
 	myapp.run(host='0.0.0.0', port='8080')
